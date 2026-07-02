7dbf64590e6c8c2c4995aa416283e0d761b7e1e2
Imtiaz Ahmed <imti.imtiaz2@gmail.com>
2026-07-02 15:58:50 -0700

    pick random sound

--- main.py
+++ main.py
@@ -5,8 +5,10 @@ import random
 import time
 
 pygame.mixer.init()
-sound = pygame.mixer.Sound("sounds/bark-fart_XRsy1HE.mp3")
-sound_length = sound.get_length()
+sounds_folder = Path("sounds")
+print(f"Available sounds:")
+for sound_file in sounds_folder.glob("*.mp3"):
+    print(f" - {sound_file.name}")
 
 config_path = Path("config.json")
 
@@ -14,14 +16,20 @@ if config_path.exists():
     with open(config_path, "r") as f:
         data = json.load(f)
 else:
-    data = {"min_interval": 60, "max_interval": 300}
+    data = {"min_interval": 5, "max_interval": 10}
     with open(config_path, "w") as f:
         json.dump(data, f, indent=4)
 
 min_interval = data["min_interval"]
 max_interval = data["max_interval"]
 
-random_number = random.randint(min_interval, max_interval)
-print(f"Random number between {min_interval} and {max_interval}: {random_number}")
-sound.play()
-time.sleep(sound_length)
+while True:
+    sound_file = random.choice(list(sounds_folder.glob("*.mp3")))
+    print(f"Playing sound: {sound_file.name}")
+    pygame.mixer.music.load(sound_file)
+    pygame.mixer.music.play()
+    while pygame.mixer.music.get_busy():
+        time.sleep(0.1)
+    interval = random.randint(min_interval, max_interval)
+    print(f"Waiting for {interval} seconds before playing the next sound.")
+    time.sleep(interval)
